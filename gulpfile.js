1b61fe21e89eea8588951c771239c3115aa70a05
Brandon Tate <blt900@gmail.com>
2015-02-21 17:28:40 -0600

    Changed file structure and added more tasks to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,48 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')(),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
 
-gulp.task('default', function() {
-    // place code for your default task here    
+gulp.task('default', ['serve', 'sass', 'js'], function() {
+    gulp.watch('js/*.js', ['compress']);
+    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch(['*.html', 'styles/**/*.css', 'js/**/*.js'], {cwd: 'app'}, reload);
 });
 
+//Sass-CSS
+gulp.task('sass', function() {
+    return $.rubySass('scss/main.scss')
+        .pipe(gulp.dest('app/styles'))
+        .pipe($.rename('main.css'))
+        .pipe(reload({ stream:true }));
+});
 
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-
-// watch files for changes and reload
 gulp.task('serve', function() {
     browserSync({
       server: {
       baseDir: 'app'
       }
-    });
-
-    gulp.watch(['*.html', 'styles/**/*.css', 'js/**/*.js'], {cwd: 'app'}, reload);
-                                                          
+    });                                                    
 });
 
-var gp_uglify = require('gulp-uglify');
-var gp_rename = require('gulp-rename');
-var gp_concat = require('gulp-concat');
-
-gulp.task('add_bc', function() {
-     gulp.src(['bower_components/jquery/dist/jquery.min.js','bower_components/angular/angular.min.js', 'bower_components/bootstrap/dist/js/bootstrap.min.js']) 
-        .pipe(gulp.dest('app/js'))
-  });
-
- 
-gulp.task('compress', function() {
-     gulp.src(['bower_components/jquery/dist/jquery.min.js','bower_components/angular/angular.min.js', 'bower_components/bootstrap/dist/js/bootstrap.min.js', 'app/js/*.js'])
-         .pipe(gp_concat('concat.js'))
+//Concat-Uglify Javascript
+gulp.task('js', function() {
+     gulp.src('js/main.js')
+         .pipe($.concat('main.concat.js'))
          .pipe(gulp.dest('dist'))
-         .pipe(gp_rename('uglify.js'))
-         .pipe(gp_uglify())
+         .pipe($.rename('main.min.js'))
+         .pipe($.uglify())
+         .pipe(gulp.dest('app/js')),
+     gulp.src('js/controllers/*.js')
+         .pipe($.concat('controllers.concat.js'))
          .pipe(gulp.dest('dist'))
+         .pipe($.rename('controllers.min.js'))
+         .pipe($.uglify())
+         .pipe(gulp.dest('app/js/controllers')),
+     gulp.src('js/factories/*.js')
+         .pipe($.concat('factories.concat.js'))
+         .pipe(gulp.dest('dist'))
+         .pipe($.rename('factories.min.js'))
+         .pipe($.uglify())
+         .pipe(gulp.dest('app/js/factories'))
  });
---
+++ js/main.js
@@ -0,0 +1,9 @@
+;(function(){
+  
+  angular.module("PinfallApp", [])
+    .controller("SimpleController", function() {
+      this.name = "Brandon Tate",
+      this.wrestlers = ["The Rock", "The Undertaker", "Billy Gunn"]
+      });
+
+  }());
